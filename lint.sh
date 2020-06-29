0a93862fb0edad93e2860baedcc2190048738e81
pushittoprod <pushittoprod@gmail.com>
2020-06-29 06:23:10 -0700

    add a script for linting

---
+++ lint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Lint all the shell scripts in this directory.
+
+declare -a shell_scripts=()
+
+# Since the scripts are named without extensions, we have to detect them using
+# `file` instead.
+for f in *; do
+  if [[ "$(file "$f")" == *Bourne-Again* ]]; then
+    shell_scripts+=("$f")
+  fi
+done
+
+shellcheck "${shell_scripts[@]}"
