8f17576cb64adbfb2da772ef905bbbb9da8d74b5
pushittotprod <pushittoprod@gmail.com>
2022-01-02 13:36:13 -0800

    confirm expenses match budgets - hacky, but works

--- scratch/validate_transactions.py
+++ scratch/validate_transactions.py
@@ -3,27 +3,48 @@ from iter_transactions import iter_transactions, print_transaction
 
 def negate(amount):
     """
-    >>> negate('$100.00')
-    '$-100.00'
-    >>> negate('$-100.00')
-    '$100.00'
+    >>> negate('100.00')
+    '-100.00'
+    >>> negate('-100.00')
+    '100.00'
     """
-    if amount.startswith('$-'):
-        return '$' + amount[2:]
+    if amount.startswith('-'):
+        return amount[1:]
     else:
-        return '$-' + amount[1:]
+        return '-' + amount
 
 
 def validate_transaction(lines):
     expenses = {}
     budgets = {}
-    for line in lines:
-        account = line['account']
-        amount = line['amount']
-        if account.startswith('(Budget:'):
-            if account[-1] != ')':
-                yield f"Missing ')' for account '{account}'"
-
+    try:
+        for line in lines:
+            account = line['account']
+            amount = line['amount']
+            if account.startswith('(Budget:'):
+                if account[-1] != ')':
+                    yield f"Missing ')' for account '{account}'"
+                    # add the missing paren for ease of use
+                    account += ')'
+                expense_account = 'Expenses:' + account[8:-1]
+                if expense_amount := expenses.get(expense_account):
+                    if negate(expense_amount) != amount:
+                        yield f"Mismatched budget amount for {expense_account}"
+                    del expenses[expense_account]
+                else:
+                    budgets[account[1:-1]] = amount
+            elif account.startswith('Expenses:'):
+                budget_account = 'Budget:' + account[9:]
+                if budget_amount := budgets.get(budget_account):
+                    if negate(budget_amount) != amount:
+                        yield f"Mismatched budget amount for {account}"
+                    del budgets[budget_account]
+                else:
+                    expenses[account] = amount
+    except:
+        print("Error processing transaction")
+        print_transaction(lines)
+        raise
 
 def main():
     for txnidx, lines in iter_transactions():
